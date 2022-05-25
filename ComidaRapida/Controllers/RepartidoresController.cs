0d1f380033dfa1936c521578de8a07e776e47390
laur73 <jj.c.r.1781@gmail.com>
2022-05-25 09:47:23 -0500

    CRUD Repartidores Agregado

---
+++ ComidaRapida/Controllers/RepartidoresController.cs
@@ -0,0 +1,115 @@
+﻿using ComidaRapida.Models;
+using ComidaRapida.Repositorios;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ComidaRapida.Controllers
+{
+    public class RepartidoresController : Controller
+    {
+        private readonly IRepositorioRepartidores repositorioRepartidores;
+
+        public RepartidoresController(IRepositorioRepartidores repositorioRepartidores)
+        {
+            this.repositorioRepartidores = repositorioRepartidores;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Listar()
+        {
+            var repartidores = await repositorioRepartidores.Listar();
+            return View(repartidores);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> VerificarExisteVendedor(string nombre, string usuario)
+        {
+            var existeRepartidor = await repositorioRepartidores.Existe(nombre, usuario);
+
+            if (existeRepartidor)
+            {
+                return Json($"El repartidor ya existe");
+            }
+
+            return Json(true);
+        }
+
+        [HttpGet]
+        public IActionResult Crear()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Crear(UsuarioViewModel repartidor)
+        {
+            //Valida los campos del modelo
+            if (!ModelState.IsValid)
+            {
+                return View(repartidor);
+            }
+
+            //Valida si ya existe el apoyo a insertar
+            var existeVendedor = await repositorioRepartidores.Existe(repartidor.Nombre, repartidor.Usuario);
+
+            if (existeVendedor)
+            {
+                ModelState.AddModelError(nameof(repartidor.Nombre), $"El vendedor ya se encuentra registrado");
+
+                return View(repartidor);
+            }
+
+            await repositorioRepartidores.Crear(repartidor);
+
+            return RedirectToAction("Listar");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Editar(int IdUsuario)
+        {
+            var repartidor = await repositorioRepartidores.ObtenerId(IdUsuario);
+
+            if (repartidor is null)
+            {
+                return RedirectToAction("NoEncontrado", "Home");
+            }
+
+            return View(repartidor);
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Editar(UsuarioViewModel repartidor)
+        {
+            var vendedorExiste = await repositorioRepartidores.ObtenerId(repartidor.IdUsuario);
+
+            if (vendedorExiste is null)
+            {
+                return RedirectToAction("NoEncontrado", "Home");
+            }
+
+            await repositorioRepartidores.Actualizar(repartidor);
+
+            return RedirectToAction("Listar");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Eliminar(int IdUsuario)
+        {
+            var repartidor = await repositorioRepartidores.ObtenerId(IdUsuario);
+
+            if (repartidor is null)
+            {
+                return RedirectToAction("NoEncontrado", "Home");
+            }
+
+            return View(repartidor);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Eliminar(UsuarioViewModel repartidor)
+        {
+            await repositorioRepartidores.Eliminar(repartidor.IdUsuario);
+            return RedirectToAction("Listar");
+        }
+    }
+}
---
+++ ComidaRapida/Repositorios/RepositorioRepartidores.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Data.SqlClient;
+using ComidaRapida.Models;
+using Dapper;
+
+namespace ComidaRapida.Repositorios
+{
+    public interface IRepositorioRepartidores
+    {
+        Task Actualizar(UsuarioViewModel repartidor);
+        Task Crear(UsuarioViewModel repartidor);
+        Task Eliminar(int IdRepartidor);
+        Task<bool> Existe(string nombre, string usuario);
+        Task<IEnumerable<UsuarioViewModel>> Listar();
+        Task<UsuarioViewModel> ObtenerId(int IdRepartidor);
+    }
+
+    public class RepositorioRepartidores : IRepositorioRepartidores
+    {
+        private readonly string connectionString;
+
+        public RepositorioRepartidores(IConfiguration configuration)
+        {
+            connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public async Task<IEnumerable<UsuarioViewModel>> Listar()
+        {
+            using var connection = new SqlConnection(connectionString);
+            return await connection.QueryAsync<UsuarioViewModel>(@"SELECT IdUsuario, Nombre, Usuario, FechaAlta FROM Usuarios WHERE roleId = 3");
+        }
+
+        public async Task<bool> Existe(string nombre, string usuario)
+        {
+            using var connection = new SqlConnection(connectionString);
+            var existe = await connection.QueryFirstOrDefaultAsync<int>(
+                @"SELECT 1 FROM Usuarios WHERE Nombre = @Nombre AND Usuario = @Usuario", new { nombre, usuario });
+            return existe == 1;
+        }
+
+        public async Task Crear(UsuarioViewModel repartidor)
+        {
+            using var connection = new SqlConnection(connectionString);
+            var id = await connection.QuerySingleAsync<int>(@"INSERT INTO Usuarios (Nombre, Usuario, Pwd, FechaAlta, RoleId)
+                                                        VALUES (@Nombre, @Usuario, @Pwd, GETDATE(),3);    
+                                                        SELECT SCOPE_IDENTITY();", repartidor);
+            repartidor.IdUsuario = id;
+        }
+
+        public async Task<UsuarioViewModel> ObtenerId(int IdRepartidor)
+        {
+            using var connection = new SqlConnection(connectionString);
+            return await connection.QueryFirstOrDefaultAsync<UsuarioViewModel>(
+                                    @"SELECT * FROM Usuarios WHERE IdUsuario = @IdRepartidor", new { IdRepartidor });
+        }
+
+        public async Task Actualizar(UsuarioViewModel repartidor)
+        {
+            using var connection = new SqlConnection(connectionString);
+            await connection.ExecuteAsync(@"UPDATE Usuarios SET Nombre = @Nombre, Usuario = @Usuario WHERE IdUsuario = @IdUsuario", repartidor);
+        }
+
+        public async Task Eliminar(int IdRepartidor)
+        {
+            using var connection = new SqlConnection(connectionString);
+            await connection.ExecuteAsync(@"DELETE Usuarios WHERE IdUsuario = @IdRepartidor", new { IdRepartidor });
+        }
+    }
+}
